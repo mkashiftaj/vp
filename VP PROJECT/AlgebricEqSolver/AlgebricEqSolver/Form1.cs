bc32b483188b83b1125e61105fbb50a1bbf944d6
mkashiftaj <kashisatti143@gmail.com>
2016-04-21 04:30:24 +0500

    new

--- VP PROJECT/AlgebricEqSolver/AlgebricEqSolver/Form1.cs
+++ VP PROJECT/AlgebricEqSolver/AlgebricEqSolver/Form1.cs
@@ -10,16 +10,43 @@ using System.Windows.Forms;
 
 namespace AlgebricEqSolver
 {
-    public partial class Form1 : Form
+    public partial class slForm : Form
     {
-        public Form1()
+        public slForm()
         {
             InitializeComponent();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {}
+
+        private void btnok_Click(object sender, EventArgs e)
         {
-            comboBox1.Items.Add("a2" + "\x00B2");
+            if (selcomb.SelectedIndex == 0)
+            {
+                QEqform obj1 = new QEqform();
+                obj1.Show();
+            }
+            if (selcomb.SelectedIndex == 1)
+            {
+                LEqform obj3 = new LEqform();
+                obj3.Show();
+            }
+            if (selcomb.SelectedIndex == 2)
+            {
+                Valuesform obj2 = new Valuesform();
+                obj2.Show();
+            }
+            if (selcomb.SelectedIndex == 3)
+            {
+                Valuesform obj3 = new Valuesform();
+                obj3.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
